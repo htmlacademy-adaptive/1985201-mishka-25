20930cef978da4a67f43535ab98b03f70d7bd275
Ирина <irene7494@mail.ru>
2022-05-22 02:28:46 +0300

    Подгонка проекта для сдачи на допуск

---
+++ source/js/modal.js
@@ -0,0 +1,12 @@
+let modal = document.querySelector('.modal');
+let cartLink = document.querySelector('.navigation__link--cart');
+
+cartLink.addEventListener('click', function () {
+  if (modal.classList.contains('modal--closed')) {
+    modal.classList.remove('modal--closed');
+    modal.classList.add('modal--opened');
+  } else {
+    modal.classList.add('modal--closed');
+    modal.classList.remove('modal--opened');
+  }
+});
